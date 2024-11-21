0e923bd50c56e361e4f6064d4e0eec3339f1513f
Omonboy <yaxshimuratovomonboy@gmail.com>
2024-11-22 00:01:00 +0500

    My first Api

---
+++ Entities/Product.cs
@@ -0,0 +1,19 @@
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+    public EProductStatus Status { get; set; }
+
+    // faqatgina navigatsiya uchun ishlatiladi
+    public ProductDetail ProductDetail { get; set; }
+}
+
+public enum EProductStatus
+{
+    Inactive,
+    Active,
+    Soldout
+}
